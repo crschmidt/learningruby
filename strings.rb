eea636c8cfcf4ebcc55dc3dce69d1b308dd0b7dd
Christopher Schmidt <christopher.schmidt@here.com>
2013-11-04 20:49:21 -0500

    More string experiments.
    Single-quotes are uninterpolated; double quotes are interpolated; ruby
    1.8 is silly and that's what OS X has by default. haxx.

--- strings.rb
+++ strings.rb
@@ -1,3 +1,10 @@
 a = "abc"
-
 puts a[1] # 98?! 98 is not a character! why do you do this to me
+puts a[1..1] # <+dpk> iirc this was fixed in Ruby 1.9
+             # <+dpk> in 1.8 you can do a[1..1] to get a character instead of a number
+
+b='\t'
+puts b
+
+c = "\tc"
+puts c
